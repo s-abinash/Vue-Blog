d048900bea7666606ddad364e071f0a4082aaf9d
Abinash S <s.abinash@outlook.com>
2020-09-12 09:51:30 +0530

    Changes

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 // vue.config.js file to be place in the root of your repository
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/Vue-Blog/'
-      : '/'
-  }
+    publicPath: '/Vue-Blog/'
+    }
